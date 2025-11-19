eaf9b6494023c30ffb013a7a0e7fd5855299262a
oskarcai <oskarcaicai@gmail.com>
2025-11-20 00:23:34 +0100

    h1

---
+++ src/h1/H1_main.java
@@ -0,0 +1,19 @@
+package h1;
+
+public class H1_main {
+    public static void main(String[] args) {
+        int[] myArray = {5,6,7,10};
+
+        // Array-Reihenfolge umdrehen:
+        for(int i = 0; i < myArray.length/2; i++) {
+            int temp = myArray[i];
+            myArray[i] = myArray[(myArray.length - 1) - i];
+            myArray[(myArray.length - 1) - i] = temp;
+        }
+
+        // Ausgabe vom Array - reversed:
+        for(int i = 0; i < myArray.length; i++) {
+            System.out.print(myArray[i] + " ");
+        }
+    }
+}
